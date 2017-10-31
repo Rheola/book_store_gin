b85c87c81dbe445aa78566579c18e1f1ef9fb308
Svetlana Okuneva <okunewa@yandex.ru>
2017-11-01 00:08:22 +0300

    Поправлена структура Автор

--- models/author.go
+++ models/author.go
@@ -2,7 +2,7 @@ package models
 
 type Author struct {
 	Id   int64  `sql:"primary_key:yes;"`
-	Name string `sql:"type:text;" json:"content" form:"content"`
+	Name string `sql:"type:text;" json:"content" form:"name"`
 }
 
 func (Author) TableName() string {
