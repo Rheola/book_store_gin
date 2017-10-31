5f4303c3646072af840b07c13d56b0b2371e2bf1
Svetlana Okuneva <okunewa@yandex.ru>
2017-10-31 22:33:03 +0300

    Main.go adminIndex - Главная админки

--- main.go
+++ main.go
@@ -2,25 +2,35 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"text/template"
 )
 
-// This function's name is a must. App Engine uses it to drive the requests properly.
+// This function's name is a must.
+// App Engine uses it to drive the requests properly.
 func main() {
-	// Starts a new Gin instance with no middle-ware
+	// Starts a new Gin instance
 	r := gin.New()
+	r.Static("/css", "./css")
+	r.Static("/font-awesome", "./font-awesome")
+	r.Static("/js", "./js")
 
-	// Define your handlers
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World!")
-	})
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+	r.GET("/admin", adminIndex)
 
 	// Handle all requests using net/http
 	http.Handle("/", r)
-
 	r.Run(":8080")
+}
+
+func adminIndex(c *gin.Context) {
+	tmpl, err := template.ParseFiles("templates/layouts/admin.html", "templates/admin/index.html")
+	if err != nil {
+		c.String(500, "Внутренняя ошибка сервера")
+	}
+
+	obj := gin.H{"title": "Главная"}
+
+	if err := tmpl.Execute(c.Writer, obj); err != nil {
+		c.String(500, "Внутренняя ошибка сервера")
+	}
 }
